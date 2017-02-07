9043ab8dcfba438e0cc6b31207cc6de73275ce69
m@g <mattghall@live.com>
2017-02-06 20:11:04 -0800

    Change from Jade to Html* hopeully fingers crossed

--- Visual Studio Project/savour-app/routes/index.js
+++ Visual Studio Project/savour-app/routes/index.js
@@ -1,9 +1,14 @@
 ﻿var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
-});
+///* GET home page. */
+//router.get('/', function (req, res) {
+//    res.render('index', { title: 'Express' });
+//});
+
+// ./routes/index.js
+exports.index = function (req, res) {
+    res.render('index', { title: 'ejs' });
+};
 
 module.exports = router;
