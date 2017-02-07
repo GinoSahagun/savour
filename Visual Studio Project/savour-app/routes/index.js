2775a618831fb819f455e337f6599f36338e1b21
m@g <mattghall@live.com>
2017-02-06 20:50:35 -0800

    Revert "Change from Jade to Html* hopeully fingers crossed"
    This reverts commit a0309e407eddfbae78cc7c3e7ccd944fbcfe8f0c.

--- Visual Studio Project/savour-app/routes/index.js
+++ Visual Studio Project/savour-app/routes/index.js
@@ -1,14 +1,9 @@
 ﻿var express = require('express');
 var router = express.Router();
 
-///* GET home page. */
-//router.get('/', function (req, res) {
-//    res.render('index', { title: 'Express' });
-//});
-
-// ./routes/index.js
-exports.index = function (req, res) {
-    res.render('index', { title: 'ejs' });
-};
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
 
 module.exports = router;
