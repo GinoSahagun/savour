94b52548c1d9c604eb16d181340a00be6ce37175
Gino <sahagun.gino@gmail.com>
2017-02-14 01:14:56 -0800

    added restaurant page and updated about page

---
+++ Visual Studio Project/savour-app/routes/aboutRestaurants.js
@@ -0,0 +1,13 @@
+﻿/**
+ * Created by CAR on 2/11/2017.
+ */
+var express = require('express');
+var router = express.Router();
+
+/* GET about page. */
+router.get('/', function (req, res) {
+    res.render('restaurantDesc', { title: 'Express' });
+    //res.res('about', { title: 'Express' });
+});
+
+module.exports = router;
