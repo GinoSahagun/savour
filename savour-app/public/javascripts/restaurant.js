5558221c2288173b55185d1a6addc310ee700777
Gino <sahagun.gino@gmail.com>
2017-03-02 00:02:22 -0800

    added restaurant javascript file load function

---
+++ savour-app/public/javascripts/restaurant.js
@@ -0,0 +1,22 @@
+﻿
+//Document Ready Function
+$(document).ready(function () {
+
+    //function load for span tags
+    $(function () {
+    //source image
+        $('#restImage').attr('src', 'images/curbside.jpg');
+        //Restaurant Name
+        $("#restName").text("Curbside");
+        //Restuaran Rating Stars
+        $("#restStars").text("5");
+        //Start Time
+        $("#startHour").text("10:00am");
+        //End Time
+        $("#endHour").text("9:00pm");
+        $("#bio").text("Fairly quick, super cheap, and very very delicious. Staff is very friendly!  Definetly a new lunch spot!👍"); 
+    });
+
+
+
+});
