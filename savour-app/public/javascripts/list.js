8267e3d3cfda670a17c0f9df7dc448a912ce755d
m@g <mattghall@live.com>
2017-03-07 00:44:54 -0800

    Links on menu go to actual result

--- savour-app/public/javascripts/list.js
+++ savour-app/public/javascripts/list.js
@@ -1,7 +1,7 @@
 ﻿$(function () {
     var tbl = $("#dashboard-list");
 
-    var data = [JSON.parse('{"name":"Curbside", "rating": "****", "desc": "Fairly quick, super cheap, and very very delicious. Staff is very friendly! Definitely a new lunch spot!"}'), JSON.parse('{"name":"Tacobell", "rating": "****", "desc": "Fairly quick, super cheap, and very very delicious. Staff is very friendly! Definitely a new lunch spot!"}'), JSON.parse('{"name":"McDonalds", "rating": "****", "desc": "Fairly quick, super cheap, and very very delicious. Staff is very friendly! Definitely a new lunch spot!"}')];
+    var data = [JSON.parse('{"name":"Curbside", "rating": "****", "desc": "Fairly quick, super cheap, and very very delicious. Staff is very friendly! Definitely a new lunch spot!","id":"58be70523ef171a24f518ab0"}'), JSON.parse('{"name":"El Camion", "rating": "****", "desc": "Fairly quick, super cheap, and very very delicious. Staff is very friendly! Definitely a new lunch spot!", "id":"58b9c9a4bd72fa71b8ba1dad"}'), JSON.parse('{"name":"McDonalds", "rating": "****", "desc": "Fairly quick, super cheap, and very very delicious. Staff is very friendly! Definitely a new lunch spot!"}')];
 
     for (d of data) {
         var row = CreateRow(d);
@@ -10,8 +10,10 @@
 });
 
 function CreateRow(data) {
-    var row = "<tr><td><div class='col-md-10'>";
-    row += data.name + "</div ></td ><td><div class='col-md-2'>" + data.rating + "</div></td></tr>";
+    if (data.id == null) 
+        data.id = "";
+    var row = "<tr><td><a href=./restaurant?id=" + data.id + "><div class='col-md-10'>";
+    row += data.name + "</div ></td ><td><div class='col-md-2'>" + data.rating + "</div></td></a></tr>";
     row += "<tr><td colspan = '2'><div class='col-md-12'>";
     row += data.desc + "</div ></td> </tr"; 
     return row;
